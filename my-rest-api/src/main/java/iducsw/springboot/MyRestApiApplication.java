1126995f48c187adb8530a1952fbef16231e425f
이민욱 <56884540+zkvn99@users.noreply.github.com>
2023-03-23 20:06:58 +0900

    [Init]

---
+++ my-rest-api/src/main/java/iducsw/springboot/MyRestApiApplication.java
@@ -0,0 +1,13 @@
+package iducsw.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRestApiApplication.class, args);
+    }
+
+}
